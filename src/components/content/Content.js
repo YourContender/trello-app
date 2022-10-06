345040a3b8d7f52988efead64f313bc4901ba4c8
YourContender <dream-boy123@ukr.net>
2022-10-07 00:48:19 +0300

    update card component

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -3,15 +3,47 @@ import Promo from './promo/Promo';
 import DeskConstructor from './desk-constructor/DeskConstructor';
 import Card from './card/Card';
 import './Content.scss';
+import { useEffect } from 'react';
 
-const Content = () => {
+function Content () {
     const [start, setStart] = useState(false);
-    const [data, setData] = useState([
-        [{
-            title: 'test'
-        }]
-    ]);
     const [displayConstructor, setDisplayConstructor] = useState(false);
+    const [data, setData] = useState([
+        {
+            title: 'create',
+            task: ['example 1', 'example 2'],
+            id: 1
+        },
+        {
+            title: 'process',
+            task: ['example 1', 'example 2', 'example 3'],
+            id: 2
+        },
+        {
+            title: 'done',
+            task: ['example 1', 'example 2'],
+            id: 3
+        },
+    ]);
+
+    const hello = (id, text) => {
+        console.log('attribute: ', text);
+        let currentElem = data.filter(item => {
+            if (item.id === id) {
+                item.task = [...item.task, ...text];
+            } 
+            return item
+        })
+
+        return setData(currentElem);
+        // return console.log('test');
+    }
+
+    console.log('1:', typeof hello);
+
+    useEffect(() => {
+
+    }, [data])
 
     return (
         <div className="content_container">
@@ -29,12 +61,10 @@ const Content = () => {
             } */}
 
             {
-                data && 
+                // data && 
                     data.map(item => {
                         return (
-                            <div>
-                                <Card item={item}/>
-                            </div>
+                            <Card item={item} func={hello}/>
                         )
                     })
             }
--- src/components/content/card/Card.js
+++ src/components/content/card/Card.js
@@ -1,39 +1,41 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEllipsis } from '@fortawesome/free-solid-svg-icons';
 import './Card.scss';
+import { useState } from 'react';
 
-const Card = ({ item }) => {
+const Card = ({item, func}) => {
     console.log('item: ', item);
-    return (
+
+    return ( 
         <div className='card_container'>
             {
-                item.map(elem => {
-                    return (
-                        <div className="card">
-                            <div className="card_header">
-                                <span>title {elem.title}</span>
-                                <a href="#">
-                                    <FontAwesomeIcon icon={faEllipsis   } />
-                                </a>
-                            </div>
-                            
-                            <div className="card_quantity">
-                                <p>quantity position: 2</p>
-                            </div>
+                <div className="card">
+                    <div className="card_header">
+                        <span>title {item.title}</span>
+                        <a href="#">
+                            <FontAwesomeIcon icon={faEllipsis   } />
+                        </a>
+                    </div>
+                    
+                    <div className="card_quantity">
+                        <p>quantity position: {item.task.length}</p>
+                    </div>
 
-                            <div className="card_content">
-                                <p>item 1 {elem.title}</p>
-                                <p>item 2 {elem.title}</p>
-                            </div>
-                            
-                            <div className="card_footer">
-                                <button>
-                                    add new position
-                                </button>
-                            </div>
-                        </div>
-                    )
-                })
+                    <div className="card_content">
+                        {
+                            item.task.map(el => {
+                                return <p>element {el}</p>
+                            })
+                        }
+                    </div>
+                    
+                    <div className="card_footer">
+                        <input type="text" placeholder="enter text"/>
+                        <button onClick={() => func(item.id, ['test1'])}>
+                            add 
+                        </button>
+                    </div>
+                </div>
             }
         </div>
     )
